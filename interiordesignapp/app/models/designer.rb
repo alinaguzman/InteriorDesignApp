6a71ea4e1f400594dbe0b849c1221dc22b2badc7
Alina Guzman <alinochka8@gmail.com>
2013-07-14 21:59:52 -0400

    Ran the db migration for all tables and updated Gemfile for deploying gems

--- interiordesignapp/app/models/designer.rb
+++ interiordesignapp/app/models/designer.rb
@@ -3,5 +3,5 @@ class Designer < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :clients
   has_and_belongs_to_many :tags
-      attr_accessible :description, :name
+  attr_accessible :description, :name
 end
--- interiordesignapp/app/models/tag.rb
+++ interiordesignapp/app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :designers
   has_and_belongs_to_many :clients
-      attr_accessible :name
+  attr_accessible :name
 end
