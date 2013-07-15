1a4c5e7a180e6e37f5ef8278bae0c38c129cbba5
Alina Guzman <alinochka8@gmail.com>
2013-07-14 21:10:52 -0400

    Created the rails app

---
+++ interiordesignapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interiordesignapp::Application.config.secret_token = 'ba094bfc2b4f751c889e0eac95eb4896a37a5daa8ffefe79856381fde7ffe138f0fea8c9803707b5e8f3873e42149955489c0cffee010bf0d911e5255f1368d9'
