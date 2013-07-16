1ce4e1c0f07d470475740d438616ee122402a59e
Alina Guzman <alinochka8@gmail.com>
2013-07-16 09:39:48 -0400

    Got the tag search to display users associated with the tags, kind of

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,28 +7,19 @@ class UsersController < ApplicationController
   end
 
   def results
-    @tags = Tag.where(name: params[:name])
-    @tags.each do |tag|
-      @designers = []
-      @designers << User.find(tag.user_id)
+    @tag_users = []
+    Tag.where(name: params[:name]).each do |tag|
+      tag.users.each do |user|
+        @tag_users << user.username
+      end
     end
-    @designers
-  end
 end
 
-
-def designer_to_tag
-  User.where(role:"Designer").each do |designer|
-    designer.tags.each do |tag|
-      puts tag.name
-    end
-  end
-end
-
-def tag_to_designer
-  Tag.where(name:"industrial").each do |tag|
-    tag.user_id.each do |user|
-      puts user.username
+  def designer_to_tag
+    User.where(role:"Designer").each do |designer|
+      designer.tags.each do |tag|
+        puts tag.name
+      end
     end
   end
 end
