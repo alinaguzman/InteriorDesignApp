3dbfb8d208744bc9f806c53391751cfcd283bb27
Alina Guzman <alinochka8@gmail.com>
2013-07-16 22:47:20 -0400

    Added a redirect on the root if user is signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,14 @@
 class WelcomeController < ApplicationController
 
   def index
-     #if params[:username]
-      #  username = params[:username]
-      #  @user = Instagram.user_search(username).first
-      #  @url = Instagram.user_recent_media(@user.id).first.images.standard_resolution.url
-     # end
-
-     # @photos = Instagram.tag_recent_media("interiordesign")
-   end
+    if current_user && current_user.role == "Client"
+      redirect_to '/users/profile'
+    elsif current_user && current_user.role == "Designer"
+      redirect_to '/designers/profile'
+    else
+      ""
+    end
+  end
 
   def instatest
     @instagram = Instagram.user_recent_media("15935796", {:count => 30})
